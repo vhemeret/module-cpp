11e9d6311c4bd07858f8705f899f821e015f7e28
vhemeret <val.kawa@icloud.com>
2022-10-19 03:07:37 +0200

    timestamp finish, function makeDeposit in progress

--- cpp00/ex02/account.cpp
+++ cpp00/ex02/account.cpp
@@ -47,18 +47,39 @@ int Account::getTotalAmount(void)
 
 void	Account::_displayTimestamp(void)
 {
-	std::time_t time = std::time(0);
+	std::time_t now = std::time(0);
+	std::tm *local = std::localtime(&now);
 
-	std::cout << "[";
+	std::cout << '[';
+	std::cout << 1900 + local->tm_year;
+	((1 + local->tm_mon) < 10) ? std::cout << '0' << 1 + local->tm_mon : std::cout << 1 + local->tm_mon;
+	(local->tm_mday < 10) ? std::cout << '0' << local->tm_mday : std::cout << local->tm_mday;
+	(local->tm_hour < 10) ? std::cout << '0' << local->tm_hour : std::cout << local->tm_hour;
+	(local->tm_min < 10) ? std::cout << '0' << local->tm_min : std::cout << local->tm_min;
+	(local->tm_sec < 10) ? std::cout << '0' << local->tm_sec : std::cout << local->tm_sec;
+	std::cout << "] ";
 }
 
 void	Account::displayAccountsInfos(void)
 {
+	_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ";" << "total:" << getTotalAmount() << ";";
 	std::cout << "deposits:" << getNbDeposits() << ";" << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << Account::_accountIndex << ';';
+	std::cout << "amount:" << Account::_amount << ';';
+	std::cout << "deposits:" << Account::_nbDeposits << ';';
+	std::cout << "withdrawals:" << Account::_nbWithdrawals << ';';
+}
+
 void	Account::makeDeposit(int deposit)
 {
+	_displayTimestamp();
+	Account::_nbDeposits += 1;
+	Account::_amount
 
 }
